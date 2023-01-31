c5fbd6f1a26ca41725a4b64857ecd4ad6bdc8b19
Kevin <kevin.castro.chavez0@gmail.com>
2023-01-31 13:29:07 -0700

    added background color to portal scene

--- src/script.js
+++ src/script.js
@@ -8,6 +8,8 @@ import { DRACOLoader } from 'three/examples/jsm/loaders/DRACOLoader.js';
  * Base
  */
 // Debug
+const debugObject = {};
+
 const gui = new dat.GUI({
   width: 400,
 });
@@ -117,6 +119,12 @@ const renderer = new THREE.WebGLRenderer({
 });
 renderer.setSize(sizes.width, sizes.height);
 renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2));
+
+debugObject.clearColor = '#201919';
+renderer.setClearColor(debugObject.clearColor);
+gui.addColor(debugObject, 'clearColor').onChange(() => {
+  renderer.setClearColor(debugObject.clearColor);
+});
 
 /**
  * Animate

3d9b4d31b3b3b4bff09c46b895f7845169dca701
Kevin <kevin.castro.chavez0@gmail.com>
2023-01-31 15:42:42 -0700

    tweaked the portal fragment to make it look more realistic

--- src/shaders/portal/fragment.glsl
+++ src/shaders/portal/fragment.glsl
@@ -80,6 +80,12 @@ float cnoise(vec3 P)
 
 void main() 
 {
-    float strength = cnoise(vec3(vUv * 5.0, uTime));
+    vec2 displacedUv = vUv + cnoise(vec3(vUv * 5.0, uTime * 0.1));
+
+    float strength = cnoise(vec3(displacedUv * 5.0, uTime * 0.2));
+
+    float outerGlow = distance(vUv, vec2(0.5)) * 5.0 - 1.4;
+    strength += outerGlow;
+
     gl_FragColor = vec4(strength, strength, strength, 1.0);
 }
